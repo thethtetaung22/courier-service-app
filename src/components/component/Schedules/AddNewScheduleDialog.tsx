93864cbe668b5b30bd0ba8708d02c9887fc44279
Thet Htet Aung <thethtetaung17@gmail.com>
2024-07-28 12:23:04 +0800

    Add new schedule ui

--- src/components/component/Schedules/AddNewScheduleDialog.tsx
+++ src/components/component/Schedules/AddNewScheduleDialog.tsx
@@ -19,9 +19,16 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
+    Input,
     Popover,
     PopoverContent,
-    PopoverTrigger
+    PopoverTrigger,
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
 } from '@/components/ui'
 import { CalendarIcon, Plus } from 'lucide-react';
 import { useForm } from 'react-hook-form';
@@ -29,9 +36,8 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
 import { SCHEDULE_STATUS } from '@/lib/enums';
-import { Schedule, Vehicle } from '@prisma/client';
+import { Schedule } from '@prisma/client';
 import { useToast } from '@/components/ui/use-toast';
-import { useRouter } from 'next/navigation';
 import { createScheduleSchema, updateScheduleSchema } from '@/schemas/schedule';
 import { format } from "date-fns"
 
@@ -44,9 +50,11 @@ const AddNewScheduleDialog = ({
     schedule?: Schedule,
     trigger?: React.ReactNode
 }) => {
-    const router = useRouter();
     const [open, setOpen] = useState(false);
-    const [color, setColor] = useState('');
+    const [hour, setHour] = useState('06');
+    const [minute, setMinute] = useState('00');
+    const [detail, setDetail] = useState('');
+
     const { toast } = useToast();
 
     const form = useForm<z.infer<typeof createScheduleSchema>>({
@@ -54,7 +62,7 @@ const AddNewScheduleDialog = ({
         defaultValues: {
             serviceDate: schedule?.serviceDate ? new Date(schedule?.serviceDate) : new Date(),
             status: schedule?.status ? SCHEDULE_STATUS[schedule?.status] : SCHEDULE_STATUS.SCHEDULED,
-            detail: schedule?.detail || '',
+            detail: schedule?.detail || 'Cleaning',
             vehicleId: schedule?.vehicleId || ''
         },
     });
@@ -119,48 +127,142 @@ const AddNewScheduleDialog = ({
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-4'>
 
+                        <div className='grid grid-cols-2 gap-2'>
+                            <FormField
+                                control={form.control}
+                                name="serviceDate"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col w-full">
+                                        <FormLabel>Service Date</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    disabled={(date) =>
+                                                        date > new Date() || date < new Date("1900-01-01")
+                                                    }
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormItem className="flex flex-col w-full">
+                                <FormLabel>Service Time</FormLabel>
+                                <div className='grid grid-cols-2 gap-2'>
+                                    <Select onValueChange={setHour} defaultValue={hour}>
+                                        <SelectTrigger className="">
+                                            <SelectValue placeholder="Select hour" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                {
+                                                    Array.from({ length: 15 }, (v, i) => (
+                                                        <SelectItem value={`${(i + 6) < 10 && '0'}${(6 + i).toString()}`}>{(i + 6) < 10 && '0'}{6 + i}</SelectItem>
+                                                    ))
+                                                }
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+
+                                    <Select onValueChange={setMinute} defaultValue={minute}>
+                                        <SelectTrigger className="">
+                                            <SelectValue placeholder="Select Minute" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                {
+                                                    Array.from({ length: 60 }, (v, i) => (
+                                                        <SelectItem value={`${i < 10 && '0'}${i}`}>{i < 10 && '0'}{i}</SelectItem>
+                                                    ))
+                                                }
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </FormItem>
+
+                        </div>
+
                         <FormField
                             control={form.control}
-                            name="serviceDate"
+                            name="detail"
                             render={({ field }) => (
                                 <FormItem className="flex flex-col w-full">
-                                    <FormLabel>Service Date</FormLabel>
-                                    <Popover>
-                                        <PopoverTrigger asChild>
-                                            <FormControl>
-                                                <Button
-                                                    variant={"outline"}
-                                                    className={cn(
-                                                        "w-[240px] pl-3 text-left font-normal",
-                                                        !field.value && "text-muted-foreground"
-                                                    )}
-                                                >
-                                                    {field.value ? (
-                                                        format(field.value, "PPP")
-                                                    ) : (
-                                                        <span>Pick a date</span>
-                                                    )}
-                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                                                </Button>
-                                            </FormControl>
-                                        </PopoverTrigger>
-                                        <PopoverContent className="w-auto p-0" align="start">
-                                            <Calendar
-                                                mode="single"
-                                                selected={field.value}
-                                                onSelect={field.onChange}
-                                                disabled={(date) =>
-                                                    date > new Date() || date < new Date("1900-01-01")
-                                                }
-                                                initialFocus
-                                            />
-                                        </PopoverContent>
-                                    </Popover>
-                                    <FormMessage />
-                                    <FormDescription>{field.value.toLocaleDateString()}</FormDescription>
+                                    <FormLabel>Service Detail</FormLabel>
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger className="">
+                                            <SelectValue placeholder="Select service detail" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectItem value={'Cleaning'}>{'Cleaning'}</SelectItem>
+                                                <SelectItem value={'Oil_Change'}>{'Oil Change'}</SelectItem>
+                                                <SelectItem value={'Tire_Replacement'}>{'Tire Replacement'}</SelectItem>
+                                                <SelectItem value={'Break_Service'}>{'Break Service'}</SelectItem>
+                                                <SelectItem value={'other'}>{'Other'}</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                    {
+                                        field.value === 'other' && <Input placeholder='Enter service detail' value={detail} onChange={e => setDetail(e.target.value)} />
+                                    }
                                 </FormItem>
                             )}
                         />
+
+                        <FormField
+                            control={form.control}
+                            name="vehicleId"
+                            render={({ field }) => (
+                                <FormItem className="flex flex-col w-full">
+                                    <FormLabel>Service Vehicle</FormLabel>
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select Vehicle" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                {
+                                                    Array.from({ length: 5 }, (v, i) => (
+                                                        <SelectItem value={`Vehicle${i.toString()}`}>{`Vehicle ${i.toString()}`}</SelectItem>
+                                                    ))
+                                                }
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                    {
+                                        field.value === 'other' && <Input placeholder='Enter service detail' value={detail} onChange={e => setDetail(e.target.value)} />
+                                    }
+                                </FormItem>
+                            )}
+                        />
+
+
+
                         <DialogFooter>
                             <DialogClose asChild>
                                 <Button variant={'link'}>Cancel</Button>
